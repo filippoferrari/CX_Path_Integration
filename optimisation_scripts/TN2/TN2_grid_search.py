73437330369b00ce9c48a092cdd9483196412ee2
filippoferrari <ferrari.filippo@outlook.com>
2020-07-15 17:57:25 +0100

    tn2 grid search

--- optimisation_scripts/TN2/TN2_grid_search.py
+++ optimisation_scripts/TN2/TN2_grid_search.py
@@ -34,7 +34,7 @@ h, v, = cx_spiking.inputs.generate_route(T_outbound=1500, vary_speed=True, route
 cx_spiking.inputs.save_route(route_file, h, v, save_route=True)
 
 # Convert headings
-headings = cx_spiking.inputs.compute_headings(h, N=N_TL2//2, vmin=5, vmax=100)
+headings = cx_spiking.inputs.compute_headings(h, N=N_TN2//2, vmin=5, vmax=100)
 headings = np.tile(headings, 2)
 
 # Convert velocity into optical flow
@@ -64,27 +64,30 @@ start_scope()
 
 time_step = 20 # ms
 
-h_stimulus = TimedArray(headings*Hz, dt=1.*time_step*ms)
-P_HEADING = PoissonGroup(N_TL2, rates='h_stimulus(t,i)')
+f_stimulus = TimedArray(flow*Hz, dt=1.*time_step*ms)
+P_FLOW = PoissonGroup(N_TN2, rates='f_stimulus(t,i)')
 
 # Neuron group
-G_TL2 = nc.generate_neuron_groups(N_TL2, eqs, threshold_eqs, reset_eqs, neuron_params, name='TL2_source_network')
+G_TN2 = nc.generate_neuron_groups(N_TN2, eqs, threshold_eqs, reset_eqs, neuron_params, name='TN2_source_network')
 
 # Add monitors
-#STM_TL2, SPM_TL2 = nc.add_monitors(G_TL2, name='TL2_source')
+# STM_TN2, SPM_TN2 = nc.add_monitors(G_TN2, name='TN2_source')
 
-# Connect heading to TL2
-S_P_HEADING_TL2 = nc.connect_synapses(P_HEADING, G_TL2, W_HEADING_TL2, model=synapses_model, 
-                                      params=synapses_params, on_pre=synapses_eqs_ex)
+# Connect heading to TN2
+S_P_FLOW_TN2 = nc.connect_synapses(P_FLOW, G_TN2, W_FLOW_TN2, model=synapses_model, 
+                                   params=synapses_params, on_pre=synapses_eqs_ex)
 
 #### Target
-TL2_spike_rates = 90 # Hz
+TN2_spike_rates_min = 50 # Hz
+TN2_spike_rates_max = 160 # Hz
+
+rescaled_tn2 = cx_log.tn2.T * (TN2_spike_rates_max - TN2_spike_rates_min) + TN2_spike_rates_min
 
 # Scale spike rates from rate-based CX in the right range
 # transpose since log is neuron_index*time_step but we want the opposite
-TL2_stimulus = TimedArray(TL2_spike_rates*cx_log.tl2.T*Hz, dt=1.*time_step*ms)
-P_TL2 = PoissonGroup(N_TL2, rates='TL2_stimulus(t,i)')
-# SPM_TL2_IDEAL = SpikeMonitor(P_TL2, name='TL2_target')
+TN2_stimulus = TimedArray(rescaled_tn2*Hz, dt=1.*time_step*ms)
+P_TN2 = PoissonGroup(N_TN2, rates='TN2_stimulus(t,i)')
+
 
 store('initialised')
 
@@ -92,10 +95,10 @@ store('initialised')
 ######################################
 ### OPTIMISER
 ######################################
-def run_simulation_TL2(tauE_, wE_, tauI_, wI_, Group, Synapses, Target,
+def run_simulation_TN2(tauE_, wE_, tauI_, wI_, Group, Synapses, Target,
                        time, dt_, delta, rate_correction): 
     restore('initialised') 
-    
+
     # set the parameters 
     Group.set_states({'tauE' : tauE_*ms,
                       'tauI' : tauI_*ms})
@@ -106,7 +109,7 @@ def run_simulation_TL2(tauE_, wE_, tauI_, wI_, Group, Synapses, Target,
     print(f'wE: {wE_} - wI {wI_}')
 
     _, model_spike_monitor = nc.add_monitors(Group)
-    target_spike_monitor = SpikeMonitor(Target, name='TL2_target_spike_monitor')
+    target_spike_monitor = SpikeMonitor(Target, name='TN2_target_spike_monitor')
     
     run(time)
 
@@ -130,7 +133,7 @@ rate_correction = True
 
 for t, tauE in enumerate(tauE_s):
     for w, wE in enumerate(wE_s):
-        gamma_factors[t,w] = run_simulation_TL2(tauE, wE, 1, 300, G_TN2,
+        gamma_factors[t,w] = run_simulation_TN2(tauE, wE, 1, 300, G_TN2,
                                                S_P_FLOW_TN2, P_TN2, T_outbound*time_step*ms, 
                                                defaultclock.dt, delta, rate_correction)
 
@@ -140,7 +143,7 @@ np.savetxt('outputs/TN2_gamma_factors_grid_search.csv', gamma_factors, delimiter
 candidate = np.argwhere(gamma_factors == np.min(gamma_factors))[0]
 
 print('Final Candidate')
-print(candidate, gamma_factors[candidate[0]], gamma_factors[candidate[[1]])
+print(candidate, gamma_factors[candidate[0]], gamma_factors[candidate[1]])
 
 ######################################
 ### TEST
@@ -150,8 +153,8 @@ start_scope()
 
 time_step = 20 # ms
 
-h_stimulus = TimedArray(headings*Hz, dt=1.*time_step*ms)
-P_HEADING = PoissonGroup(N_TL2, rates='h_stimulus(t,i)')
+f_stimulus = TimedArray(flow*Hz, dt=1.*time_step*ms)
+P_FLOW = PoissonGroup(N_TN2, rates='f_stimulus(t,i)')
 
 
 params_TL2 = neuron_params
@@ -164,25 +167,25 @@ synapses_TL2['wE'] = wE_s[candidate[1]] * nS
 # synapses_TL2['wI'] = 300 * nS
 
 
-print(params_TL2)
-print(synapses_TL2)
+print(params_TN2)
+print(synapses_TN2)
 
 
 # Neuron group
-G_TL2 = nc.generate_neuron_groups(N_TL2, eqs, threshold_eqs, reset_eqs, params_TL2, name='TL2_test')
+G_TN2 = nc.generate_neuron_groups(N_TN2, eqs, threshold_eqs, reset_eqs, params_TN2, name='TN2_test')
 
 # Add monitors
-STM_TL2, SPM_TL2 = nc.add_monitors(G_TL2, name='TL2_test')
+STM_TN2, SPM_TN2 = nc.add_monitors(G_TN2, name='TN2_test')
 
-# Connect heading to TL2
-S_P_HEADING_TL2 = nc.connect_synapses(P_HEADING, G_TL2, W_HEADING_TL2, params=synapses_TL2, model=synapses_model, on_pre=synapses_eqs_ex)
+# Connect heading to TN2
+S_P_FLOW_TN2 = nc.connect_synapses(P_FLOW, G_TN2, W_FLOW_TN2, params=synapses_TN2, 
+                                   model=synapses_model, on_pre=synapses_eqs_ex)
 
 
 # Run simulation
 run(T_outbound*time_step*ms)
 
-cx_spiking.plotting.plot_rate_cx_log_spikes(cx_log.tl2, TL2_spike_rates, SPM_TL2, 
+cx_spiking.plotting.plot_rate_cx_log_spikes(cx_log.tn2, TN2_spike_rates, SPM_TN2, 
                                             time_step, figsize=(13,8), savefig_='plots/TN2_grid_search.pdf')
-
 cx_spiking.plotting.plot_gamma_factors(gamma_factors, tauE_s, wE_s, 
                                        figsize=(11,7), savefig_='plots/TN2_gamma_factors_grid_search.pdf'):
