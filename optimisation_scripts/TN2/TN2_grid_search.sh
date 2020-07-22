a66dc71e96c11ca6796f49e4b1e46143e900f833
f.ferrari <f.ferrari@sms.ed.ac.uk>
2020-07-22 11:15:26 +0100

    new tn2 grid search results

--- optimisation_scripts/TN2/TN2_grid_search.sh
+++ optimisation_scripts/TN2/TN2_grid_search.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ~/.bashrc
 conda activate py_msc
-nice -n 12 python ./TN2_grid_search.py > outputs/TN2_grid_search.out
+nice -n 12 python ./TN2_grid_search.py > outputs_new/TN2_grid_search.out
